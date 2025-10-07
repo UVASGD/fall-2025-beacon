4a2fb50ef2465b98b6c819c36fbd72722f38e0fe
David K <davidknight11207@gmail.com>
2025-10-06 23:13:02 -0400

    Scaling construction borders
    Construction borders are now a single sprite, and scale appropriately for randomly generated captured asteroids/moons

--- Assets/Scripts/MapGen/SpawnedAsteroid.cs
+++ Assets/Scripts/MapGen/SpawnedAsteroid.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnedAsteroid : MonoBehaviour
 {
     //data that contains information about a spawned-in asteroid
     [SerializeField] OrbitalData orbitalData;
+    [SerializeField] SpriteRenderer mainRenderer;
+    [SerializeField] BoxCollider buildableArea;
+    [SerializeField] SpriteRenderer buildableAreaRenderer;
     public OrbitalData OrbitalData => orbitalData;
 
     public void InitializeAsteroid(float asteroidScale)
     {
-        SpriteRenderer renderer = GetComponentInChildren<SpriteRenderer>();
         //renderer.gameObject.transform.localScale = new Vector3(asteroidScale, 1, asteroidScale); //changing this scale also alters the size of the construction box.
 
         int spriteIndex = 0;
@@ -36,6 +39,10 @@ public class SpawnedAsteroid : MonoBehaviour
                 spriteIndex = 5;
                 break;
         }
-        renderer.sprite = AsteroidVarieties.i.AsteroidSprites[spriteIndex];
+        mainRenderer.sprite = AsteroidVarieties.i.AsteroidSprites[spriteIndex];
+
+        //set the size of the buildableArea and its renderer
+        buildableArea.size = Vector3.one * spriteIndex * 4;
+        buildableAreaRenderer.gameObject.transform.localScale = Vector3.one * spriteIndex;
     }
 }
